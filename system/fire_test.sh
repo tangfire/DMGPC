c291f3ed5359900fc55e374905a85de658f976d2
tangfire <52628216@qq.com>
2025-03-04 09:11:19 +0800

    good morning!!

--- system/fire_test.sh
+++ system/fire_test.sh
@@ -5,3 +5,5 @@ python -u main.py -t 1 -ab 1 -lr 0.01 -jr 1 -lbs 10 -ls 1 -nc 20 -ncl 100 -data 
 python -u main.py -t 1 -ab 1 -lr 0.01 -jr 1 -lbs 10 -ls 1 -nc 20 -ncl 100 -data Cifar100 -m CNNP-5 -fd 512 -did 0 -algo FedMRL -lam 10
 
 python -u main.py -t 1 -ab 1 -lr 0.01 -jr 1 -lbs 10 -ls 1 -nc 20 -ncl 100 -data Cifar100 -m CNNP-5 -fd 512 -did 0 -algo FedTGP -lam 10
+
+python -u main.py -t 1 -ab 1 -lr 0.01 -jr 1 -lbs 10 -ls 1 -nc 20 -ncl 100 -data Cifar100 -m CNNP-5 -fd 512 -did 0 -algo FedGH -lam 10
